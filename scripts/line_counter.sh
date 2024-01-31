155ca4522347d80891b27cd69dad24b2dbfe3b2f
Matteo <matteo.earendil.marinelli@gmail.com>
2024-01-31 12:12:45 +0100

    Updated: script

--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "#Test result" >> README.md
+
 LINES=$(cat main.cpp | wc -l)
-echo "Lines in main.cpp: $LINES"
+echo "Lines in main.cpp: $LINES" >> README.md
 SRC_COUNT=$(cat *.cpp | wc -l)
-echo "lines in code base: $SRC_COUNT"
+echo "lines in code base: $SRC_COUNT" >> README.md
