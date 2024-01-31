b58e124c98bc377a147fb91798b2044db9a10a29
Matteo <matteo.earendil.marinelli@gmail.com>
2024-01-31 12:55:22 +0100

    Changed: renamed template

--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -2,4 +2,4 @@
 
 SRC_LINE_COUNT=$(cat *.cpp | wc -l)
 echo -e "SRC_LINE_COUNT: $SRC_LINE_COUNT"
-sed  -e "s/__LINES__/${SRC_LINE_COUNT}/" scripts/report-template.md > README.md
+sed  -e "s/__LINES__/${SRC_LINE_COUNT}/" scripts/README-template.md > README.md
