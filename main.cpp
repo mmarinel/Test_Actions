5fe3082dcfe14dae21f9647cde7422e665552f28
Matteo <matteo.earendil.marinelli@gmail.com>
2024-01-31 11:34:13 +0100

    Added: hello world main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-31
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
