0df979c5d70a61daf67195cedfaf83cca943c19f
Matteo <matteo.earendil.marinelli@gmail.com>
2024-01-31 12:31:21 +0100

    Updated: script

--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "#Test_Actions\n" > README.md
-echo -e "#Test result\n" >> README.md
+echo -e "##Test result\n" >> README.md
 
 LINES=$(cat main.cpp | wc -l)
 echo -e "Lines in main.cpp: $LINES\n" >> README.md
