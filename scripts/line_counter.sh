d135d87c22faf37742af8cedd5a23cd0890bf317
Matteo <matteo.earendil.marinelli@gmail.com>
2024-01-31 12:49:54 +0100

    Fixed: script

--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -2,4 +2,4 @@
 
 SRC_LINE_COUNT=$(cat *.cpp | wc -l)
 echo -e "SRC_LINE_COUNT: $SRC_LINE_COUNT"
-sed  -e "s/__LINES__/${SRC_LINE_COUNT}/" script/report-template.md > README.md
+sed  -e "s/__LINES__/${SRC_LINE_COUNT}/" scripts/report-template.md > README.md
